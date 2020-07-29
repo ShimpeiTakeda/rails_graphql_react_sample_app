465141e94f3cebb068ccd73310571f71f8d20ca8
Shimpei Takeda <take9130@gmail.com>
2020-07-30 08:36:40 +0900

    setup

--- graphql_api/db/schema.rb
+++ graphql_api/db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_28_233733) do
+ActiveRecord::Schema.define(version: 2020_07_29_233238) do
 
   create_table "books", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "cover_url"
+    t.integer "average_rating"
     t.index ["user_id"], name: "index_books_on_user_id"
   end
 
