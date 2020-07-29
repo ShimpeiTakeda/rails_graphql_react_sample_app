14795dfbcc863d6b034973451bc558ff31ccbaa7
Shimpei Takeda <take9130@gmail.com>
2020-07-29 13:39:42 +0900

    Finish Backend

--- graphql_api/app/graphql/types/book_type.rb
+++ graphql_api/app/graphql/types/book_type.rb
@@ -2,7 +2,7 @@ module Types
   class BookType < Types::BaseObject
     field :id, ID, null: false
     field :user_id, Integer, null: false
-    field :title, String, null: true
+    field :title, String, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
--- graphql_api/app/graphql/types/user_type.rb
+++ graphql_api/app/graphql/types/user_type.rb
@@ -1,9 +1,17 @@
+# frozen_string_literal: true
+
 module Types
   class UserType < Types::BaseObject
     field :id, ID, null: false
     field :email, String, null: true
     field :name, String, null: true
+    field :books, [Types::BookType], null: true
+    field :books_count, Integer, null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+
+    def books_count
+      object.books.size
+    end
   end
 end
